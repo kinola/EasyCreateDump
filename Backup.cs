3a92404940fdd6a63bd01ab20b2b4097e51b9eff
kinola <nicolas.dobignard@gmail.com>
2015-12-19 13:15:16 +0100

    First commit

---
+++ Backup.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+
+namespace AesPackageFromScratch
+{
+    public class Backup
+    {
+        private string connexionString = string.Empty;
+        public string ConnexionString
+        {
+            get
+            {
+                return connexionString;
+            }
+
+            set
+            {
+                this.connexionString = value;
+            }
+        }
+        public string FileBackup
+        {
+            get
+            {
+                return fileBackup;
+            }
+
+            set
+            {
+                this.fileBackup = value;
+            }
+        }
+        private string fileBackup = string.Empty;
+
+        private MySqlConnection connexion = null;
+
+        public Backup()
+        {
+
+        }
+
+        public Backup(string connexionString, string fileBackup)
+        {
+            ConnexionString = connexionString;
+            InitConnexion();
+            FileBackup = fileBackup;
+        }
+
+        private void InitConnexion()
+        {
+            connexion = new MySqlConnection(ConnexionString);
+            try
+            {
+                connexion.Open();
+            }
+            catch (MySqlException exc)
+            {
+                throw new Exception(string.Format("Impossible d'ouvrir une connexion ({0}) : {1}", ConnexionString, exc.Message));
+            }
+        }
+
+        private List<string> GetListToExclude()
+        {
+            return new List<string>(ConfigurationManager.AppSettings["tablesToExclude"].Split(new char[] { ';' }));
+        }
+
+        private Dictionary<string, string> GetDictionaryTables(DataTable datatableAll)
+        {
+            var listToExclude = GetListToExclude();
+            var dictionaryTables = new Dictionary<string, string>();
+
+            for (int i = 0; i < datatableAll.Rows.Count; i++)
+            {
+                var table = datatableAll.Rows[i][0].ToString();
+                if (!listToExclude.Where(x => table.StartsWith(x)).ToList().Any())
+                {
+                    dictionaryTables.Add(table, string.Format("SELECT * FROM `{0}`", table));
+                }
+                else
+                {
+                    dictionaryTables.Add(table, string.Format("SELECT * FROM `{0}` where enreg=0", table));
+                }
+            }
+
+            return dictionaryTables;
+        }
+
+        public void GenerateBackup()
+        {
+            using (MySqlCommand commandSql = new MySqlCommand())
+            {
+                using (MySqlBackup backup = new MySqlBackup(commandSql))
+                {
+                    commandSql.Connection = connexion;
+                    var datatableAll = QueryExpress.GetTable(commandSql, "SHOW FULL TABLES");
+                    Dictionary<string, string> dictionaryTables = GetDictionaryTables(datatableAll);
+                    backup.ExportInfo.TablesToBeExportedDic = dictionaryTables;
+
+                    try
+                    {
+                        backup.ExportToFile(FileBackup);
+                    }
+                    catch (Exception exc)
+                    {
+                        throw new Exception(string.Format("Impossible de générer l'export dans le fichier demandé ({0}) : {1}", FileBackup, exc.Message));
+                    }
+                    finally
+                    {
+                        connexion.Close();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using MySql.Data.MySqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Data;
+using System.Configuration;
+using System.Threading.Tasks;
+using AesPackageFromScratch;
+using System.Threading;
+using System.ComponentModel;
+using System;
+using System.IO;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ComboBoxSelectionChanged(object sender, EventArgs e)
+        {
+            string connectionStringTemp = connectionString.Text;
+
+            var value = comboBox.Text;
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                var controller = await this.ShowMessageAsync("Confirmation", string.Format("Vous allez lancer le DUMP sur {0} ?", value), MessageDialogStyle.AffirmativeAndNegative);
+                if (controller == MessageDialogResult.Affirmative)
+                {
+                    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                    CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+                    Task task = Task.Factory.StartNew(() =>
+                    {
+                        var back = new Backup(string.Join(connectionStringTemp, string.Format("database={0};", value), ";"), Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, string.Format("{0}.sql", value)));
+                        back.GenerateBackup();
+                    }, cancellationToken);
+
+                    task.Wait();
+
+                    await this.ShowMessageAsync("Validation", "Export terminé dans le répertoire courant");
+                }
+            }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            connectionString.Text = ConfigurationManager.AppSettings["connectionString"];
+            LoadCombobox(true);
+        }
+
+        private async void LoadCombobox(bool firstLoad)
+        {
+            var connString = connectionString.Text;
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection(connString);
+            }
+            catch (ArgumentException)
+            {
+                await this.ShowMessageAsync("Erreur", "Attention, les informations fournies sont erronées");
+                return;
+            }
+
+            using (MySqlCommand commandSql = new MySqlCommand())
+            {
+                commandSql.Connection = connection;
+                try
+                {
+                    var datatableAll = QueryExpress.GetTable(commandSql, "show databases");
+                    comboBox.ItemsSource = datatableAll.DefaultView;
+                }
+                catch (MySqlException)
+                {
+                    await this.ShowMessageAsync("Erreur", "Attention, les informations fournies sont erronées");
+                    return;
+                }
+
+                if (!firstLoad)
+                {
+                    await this.ShowMessageAsync("Validation", "Les informations fournies sont correctes");
+                }
+            }
+
+            connection.Close();
+        }
+
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            LoadCombobox(false);
+        }
+    }
+}
